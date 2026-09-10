afe3735f0fdcf56d996bdc39bbd88dcf5440e9b4
JonatanAlberto <joalberto1@hotmail.com>
2026-09-10 14:47:16 -0300

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun findDuplicate(nums: IntArray): Int {
+        var slow = nums[0]
+        var fast = nums[0]
+        do {
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        } while (slow != fast)
+        
+        var ptr1 = nums[0]
+        var ptr2 = slow
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1]
+            ptr2 = nums[ptr2]
+        }
+        
+        return ptr1
+    }
+}
