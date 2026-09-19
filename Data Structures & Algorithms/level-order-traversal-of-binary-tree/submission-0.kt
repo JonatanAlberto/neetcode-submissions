c1cc418ec7f6b795e5af9569419864381ce25d42
JonatanAlberto <joalberto1@hotmail.com>
2026-09-19 14:01:00 -0300

    Add: level-order-traversal-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-0.kt
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun levelOrder(root: TreeNode?): List<List<Int>> {
+        val list = mutableListOf<List<Int>>()
+        if (root == null) return list
+        val queue = ArrayDeque<TreeNode?>()
+        queue.add(root)
+        while (queue.isNotEmpty()) {
+            val levelSize = queue.size
+            val level = mutableListOf<Int>()
+            repeat(levelSize) {
+                val node = queue.removeFirst()
+                if (node != null) {
+                    level.add(node.`val`)
+                    node.left?.let { queue.add(it) }
+                    node.right?.let { queue.add(it) }
+                }
+            }
+            list.add(level)
+        }
+        return list
+    }
+}
